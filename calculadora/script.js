29055212dc4f91ca675de00739f3340db4d75832
Gisele Fragoso <gisele-fragoso@outlook.com>
2022-12-15 01:03:05 +0100

    fist commit

---
+++ calculadora/script.js
@@ -0,0 +1,36 @@
+
+const calcular = document.getElementById('calcular')
+
+
+function imc() {
+    const nome = document.getElementById('nome').value;
+    const peso = document.getElementById('peso').value;
+    const altura = document.getElementById('altura').value;
+    const resultado = document.getElementById('resultado');
+
+    if (nome !== '' && altura !== '' && peso !== '') {
+    
+    const valorIMC  = (peso / (altura * altura )).toFixed(1);
+
+    let classificacao = '' ;
+
+    if (valorIMC < 18.5) {
+        classificacao = 'abaixo do peso.';
+    }
+    else if (valorIMC < 25) {
+        classificacao = 'com o peso ideal . Parabèns !!'
+    }
+    else if(valorIMC < 30) {
+        classificacao = 'levemente acima do peso '
+    }
+    else if (valorIMC < 35) {
+        classificacao = ' com obesidade. Cuidado !!'
+    }
+        resultado.textContent = `${nome}  seu IMC è ${valorIMC} e você está ${classificacao}`;
+    
+        
+    }else {
+        resultado.textContent = 'Preencha todos os campos !!'
+    }
+}
+calcular.addEventListener('click', imc);
---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+const elementosCarrosel = document.querySelector('.carrosel');
+M.Carrosel.init(elementosCarrosel, {
+    duration: 150
+}
+    );
+});
+
